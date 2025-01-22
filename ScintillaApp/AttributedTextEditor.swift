68f240cbe918b7fc476cfb072bf786c82eb976b9
quephird <danielle.kefford@gmail.com>
2025-01-21 18:33:34 -0800

    Moved highlighting logic to CodeEditor.

--- ScintillaApp/AttributedTextEditor.swift
+++ ScintillaApp/AttributedTextEditor.swift
@@ -10,9 +10,12 @@ import SwiftUI
 
 struct AttributedTextEditor: NSViewRepresentable {
     @Binding public var attributedString: NSAttributedString
+    private var highlighter: (NSTextStorage) -> Void
 
-    public init(text: Binding<NSAttributedString>) {
+    public init(text: Binding<NSAttributedString>,
+                highlighter: @escaping (NSTextStorage) -> Void) {
         self._attributedString = text
+        self.highlighter = highlighter
     }
     
     var defaultAttributes: [NSAttributedString.Key : Any] = [
@@ -21,9 +24,8 @@ struct AttributedTextEditor: NSViewRepresentable {
     ]
 
     var attributedStringWithDefaults: NSAttributedString {
-        var withDefaults = attributedString.mutableCopy() as! NSMutableAttributedString
+        let withDefaults = attributedString.mutableCopy() as! NSMutableAttributedString
         withDefaults.addAttributes(defaultAttributes, range: NSRange(location: 0, length: withDefaults.length))
-        self.addSyntaxHighlighting(attributedString: &withDefaults)
         return withDefaults
     }
 
@@ -38,6 +40,7 @@ struct AttributedTextEditor: NSViewRepresentable {
         textView.textColor = .purple
         textView.backgroundColor = NSColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
         textView.drawsBackground = true
+
         scrollView.backgroundColor = NSColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
         scrollView.drawsBackground = true
         scrollView.scrollerKnobStyle = .light
@@ -48,63 +51,7 @@ struct AttributedTextEditor: NSViewRepresentable {
         let view = (view as! NSScrollView).documentView as! AttributedTextView
         let currentCursorRange = view.selectedRanges
         view.attributedString = attributedStringWithDefaults
+        self.highlighter(view.textStorage!)
         view.selectedRanges = currentCursorRange
-    }
-
-    private func addSyntaxHighlighting(attributedString: inout NSMutableAttributedString) {
-        let code = attributedString.string
-
-        let cameraKeyword = ["Camera"]
-        let lightKeywords = ["AreaLight", "PointLight"]
-        let shapeKeywords = ["ParametricSurface", "Plane"]
-        let keywordColorMappings: [[String]: NSColor] = [
-            cameraKeyword: #colorLiteral(red: 0, green: 0.8409650922, blue: 1, alpha: 1),
-            lightKeywords: #colorLiteral(red: 0, green: 1, blue: 0.8250406384, alpha: 1),
-            shapeKeywords: #colorLiteral(red: 0, green: 1, blue: 0.5444420576, alpha: 1),
-        ]
-
-        for (keywords, color) in keywordColorMappings {
-            for keyword in keywords {
-                let attributes: [NSAttributedString.Key: Any] = [.foregroundColor: color]
-                let ranges = code.ranges(of: keyword)
-                for range in ranges {
-                    let nsRange = NSRange(range, in: code)
-                    attributedString.addAttributes(attributes, range: nsRange)
-                }
-            }
-        }
-    
-        let paramAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 0, green: 0.4649228454, blue: 0.5589954257, alpha: 1)]
-        let paramNameRegex = /\p{ID_Start}\p{ID_Continue}*(?=\s*:)/
-        let paramRanges = code.ranges(of: paramNameRegex)
-        for range in paramRanges {
-            let nsRange = NSRange(range, in: code)
-            attributedString.addAttributes(paramAttributes, range: nsRange)
-        }
-
-        let numberAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 1, green: 0.9946382642, blue: 0.3478766382, alpha: 1)]
-        let numberRegex = /-?(\d+.)?\d+/
-        let numberRanges = code.ranges(of: numberRegex)
-        for range in numberRanges {
-            let nsRange = NSRange(range, in: code)
-            attributedString.addAttributes(numberAttributes, range: nsRange)
-        }
-
-        let methodAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 0.1923640966, green: 0.5559594035, blue: 0.3243826628, alpha: 1)]
-        let methodRegex = /\.(?<method>\p{ID_Start}\p{ID_Continue}*)/
-        let methodMatches = code.matches(of: methodRegex)
-        for match in methodMatches {
-            let swiftRange = match.method.startIndex ..< match.method.endIndex
-            let nsRange = NSRange(swiftRange, in: code)
-            attributedString.addAttributes(methodAttributes, range: nsRange)
-        }
-
-        let punctuationAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)]
-        let punctuationRegex = /\.|:|\(|\)|\{|\}|,/
-        let punctuationRanges = code.ranges(of: punctuationRegex)
-        for range in punctuationRanges {
-            let nsRange = NSRange(range, in: code)
-            attributedString.addAttributes(punctuationAttributes, range: nsRange)
-        }
     }
 }
--- ScintillaApp/CodeEditor.swift
+++ ScintillaApp/CodeEditor.swift
@@ -20,6 +20,63 @@ struct CodeEditor: View {
     }
 
     var body: some View {
-        AttributedTextEditor(text: $attributedCode)
+        AttributedTextEditor(text: $attributedCode, highlighter: codeHighlighter)
+    }
+
+    public func codeHighlighter(textStorage: NSTextStorage) {
+        let code = textStorage.string
+
+        let cameraKeyword = ["Camera"]
+        let lightKeywords = ["AreaLight", "PointLight"]
+        let shapeKeywords = ["ParametricSurface", "Plane"]
+        let keywordColorMappings: [[String]: NSColor] = [
+            cameraKeyword: #colorLiteral(red: 0, green: 0.8409650922, blue: 1, alpha: 1),
+            lightKeywords: #colorLiteral(red: 0, green: 1, blue: 0.8250406384, alpha: 1),
+            shapeKeywords: #colorLiteral(red: 0, green: 1, blue: 0.5444420576, alpha: 1),
+        ]
+
+        for (keywords, color) in keywordColorMappings {
+            for keyword in keywords {
+                let attributes: [NSAttributedString.Key: Any] = [.foregroundColor: color]
+                let ranges = code.ranges(of: keyword)
+                for range in ranges {
+                    let nsRange = NSRange(range, in: code)
+                    textStorage.addAttributes(attributes, range: nsRange)
+                }
+            }
+        }
+
+        let paramAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 0, green: 0.4649228454, blue: 0.5589954257, alpha: 1)]
+        let paramNameRegex = /\p{ID_Start}\p{ID_Continue}*(?=\s*:)/
+        let paramRanges = code.ranges(of: paramNameRegex)
+        for range in paramRanges {
+            let nsRange = NSRange(range, in: code)
+            textStorage.addAttributes(paramAttributes, range: nsRange)
+        }
+
+        let numberAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 1, green: 0.9946382642, blue: 0.3478766382, alpha: 1)]
+        let numberRegex = /-?(\d+.)?\d+/
+        let numberRanges = code.ranges(of: numberRegex)
+        for range in numberRanges {
+            let nsRange = NSRange(range, in: code)
+            textStorage.addAttributes(numberAttributes, range: nsRange)
+        }
+
+        let methodAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 0.1923640966, green: 0.5559594035, blue: 0.3243826628, alpha: 1)]
+        let methodRegex = /\.(?<method>\p{ID_Start}\p{ID_Continue}*)/
+        let methodMatches = code.matches(of: methodRegex)
+        for match in methodMatches {
+            let swiftRange = match.method.startIndex ..< match.method.endIndex
+            let nsRange = NSRange(swiftRange, in: code)
+            textStorage.addAttributes(methodAttributes, range: nsRange)
+        }
+
+        let punctuationAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)]
+        let punctuationRegex = /\.|:|\(|\)|\{|\}|,/
+        let punctuationRanges = code.ranges(of: punctuationRegex)
+        for range in punctuationRanges {
+            let nsRange = NSRange(range, in: code)
+            textStorage.addAttributes(punctuationAttributes, range: nsRange)
+        }
     }
 }
