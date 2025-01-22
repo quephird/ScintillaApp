f944795199d3176999f302e9293e68bedc2df711
quephird <danielle.kefford@gmail.com>
2025-01-21 19:59:39 -0800

    Moved colors into asset catalog.

--- ScintillaApp/CodeEditor.swift
+++ ScintillaApp/CodeEditor.swift
@@ -44,28 +44,35 @@ extension CodeEditor {
         let lightKeywords = /AreaLight|PointLight/
         let shapeKeywords = /ParametricSurface|Plane/
         let regexColorMappings: [(Regex<Substring>, NSColor)] = [
-            (cameraKeyword, #colorLiteral(red: 0, green: 0.8409650922, blue: 1, alpha: 1)),
-            (lightKeywords, #colorLiteral(red: 0, green: 1, blue: 0.8250406384, alpha: 1)),
-            (shapeKeywords, #colorLiteral(red: 0, green: 1, blue: 0.5444420576, alpha: 1)),
+            (cameraKeyword, NSColor(named: "CameraKeyword")!),
+            (lightKeywords, NSColor(named: "LightKeyword")!),
+            (shapeKeywords, NSColor(named: "ShapeKeyword")!),
         ]
 
         for (regex, color) in regexColorMappings {
             self.highlight(textStorage: textStorage, regex: regex, color: color)
         }
+
     }
 
     private func highlightParameterNames(textStorage: NSTextStorage) {
         let parameterNameRegex = /\p{ID_Start}\p{ID_Continue}*(?=\s*:)/
-        self.highlight(textStorage: textStorage, regex: parameterNameRegex, color: #colorLiteral(red: 0, green: 0.4649228454, blue: 0.5589954257, alpha: 1))
+        self.highlight(textStorage: textStorage,
+                       regex: parameterNameRegex,
+                       color: NSColor(named: "ParameterName")!)
     }
 
     private func highlightNumbers(textStorage: NSTextStorage) {
         let numberRegex = /-?(\d+.)?\d+/
-        self.highlight(textStorage: textStorage, regex: numberRegex, color: #colorLiteral(red: 1, green: 0.9946382642, blue: 0.3478766382, alpha: 1))
+        self.highlight(textStorage: textStorage,
+                       regex: numberRegex,
+                       color: NSColor(named: "Number")!)
     }
 
     private func highlightMethodNames(textStorage: NSTextStorage) {
-        let methodAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: #colorLiteral(red: 0.1923640966, green: 0.5559594035, blue: 0.3243826628, alpha: 1)]
+        let methodAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: NSColor(named: "MethodName")!,
+        ]
         let methodRegex = /\.(?<method>\p{ID_Start}\p{ID_Continue}*)/
         let methodMatches = textStorage.string.matches(of: methodRegex)
         for match in methodMatches {
@@ -77,7 +84,9 @@ extension CodeEditor {
 
     private func highlightPunctuation(textStorage: NSTextStorage) {
         let punctuationRegex = /\.|:|\(|\)|\{|\}|,/
-        self.highlight(textStorage: textStorage, regex: punctuationRegex, color: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
+        self.highlight(textStorage: textStorage,
+                       regex: punctuationRegex,
+                       color: NSColor(named: "Punctuation")!)
     }
 
     private func highlight<T>(textStorage: NSTextStorage,
